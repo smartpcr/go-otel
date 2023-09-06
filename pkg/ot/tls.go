5872383e5113cc3ee687384564188bd543361371
Xiaodong Li <xiaodoli@microsoft.com>
2023-09-06 01:39:43 -0700

    log certs folder check

--- pkg/ot/tls.go
+++ pkg/ot/tls.go
@@ -3,11 +3,19 @@ package ot
 import (
 	"crypto/tls"
 	"crypto/x509"
+	"fmt"
 	"github.com/pkg/errors"
+	"log"
 	"os"
 )
 
 func getTls() (*tls.Config, error) {
+	cwd, _ := os.Getwd()
+	fmt.Println("checking certs folder in working directory:", cwd)
+	if _, err := os.Stat("./certs"); os.IsNotExist(err) {
+		log.Fatal("certs folder does not exist!")
+	}
+
 	clientAuth, err := tls.LoadX509KeyPair("./certs/client.crt", "./certs/client.key")
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to load client key pair")
